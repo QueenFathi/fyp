4370ec0aab7499dedafa4da3501a2b85631890aa
QueenFathi <fathiat477odutayo@gmail.com>
2024-07-24 13:47:25 +0100

    Initial commit

---
+++ fyp.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 17:16:55 2024
+
+@author: Hp Elitebook 1040 G3
+"""
+
+import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+tokenizer = AutoTokenizer.from_pretrained("opus")
+model = AutoModelForSeq2SeqLM.from_pretrained("opus")
+
+
+# Title of the app
+st.title('English to Yoruba Translator')
+
+# Input text box
+st.write('### TRANSCRIPT: ENGLISH')
+english_text = st.text_area('Enter English text here:', '')
+
+# Initialize Yoruba translation text area
+yoruba_text = ''
+
+# Translate button
+if st.button('Translate'):
+    # Perform translation
+    a = model.generate(**tokenizer.prepare_seq2seq_batch(english_text, return_tensors="pt"))
+    yoruba_text = tokenizer.batch_decode(a)
+
+if type(yoruba_text)==list:
+    yoruba_text=yoruba_text[0].replace("<pad>", "").replace("</s>", "").strip()
+
+# Display translation
+st.write('### TRANSLATION: YORUBA')
+st.text_area('Yoruba Translation:', yoruba_text, height=100)
